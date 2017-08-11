63955f70338df10f1761388530f2859ff8664cef
Duarti <gui13dula@gmail.com>
2017-08-11 20:06:43 -0300

    Menu change, Version change, lot o things change

--- js/default.js
+++ js/default.js
@@ -4,36 +4,13 @@ $(function () {
 
 
 function init() {
-    $("main").fullpage({
-            onLeave: (anchorIndex, index) => {
-                if(index === 5){
+    $("main").fullpage();
+    menu();
+}
 
-                  /*  $('#tempo .fp-scroller').css({
-                        transform: 'translate(0px, 21px) translateZ(0px)',
-                        "transition-timing-function": 'cubic-bezier(0.1, 0.57, 0.1, 1)',
-                        "transition-duration": '0ms',
-                        position: "relative",
-                        left: "0px",
-                        top: "0px"
-                    });
-                    $('#tempo .iScrollIndicator').css({
-                        "box-sizing": "border-box",
-                        "position": "absolute",
-                        "background": "rgba(0, 0, 0, 0.5)",
-                        "border": "1px solid rgba(255, 255, 255, 0.9)",
-                        "border-radius": "3px; width: 7px",
-                        "transition-duration": "0ms",
-                        "display": "block",
-                        "height": "674px",
-                        "transform": "translate(0px, 0px) translateZ(0px)",
-                        "transition-timing-function": "cubic-bezier(0.1, 0.57, 0.1, 1)",
-                        "z-index": "9999",
-                        "bottom": "2px",
-                        "top": "2px",
-                        "right": "1px",
-                        "overflow": "hidden"
-                    });*/
-                }
-            }
+function menu(){
+    $('#menu-icon').on('click', function (click) {
+       $('#menu-icon').toggleClass('active');
+        $('#menu').toggleClass('active');
     });
 }

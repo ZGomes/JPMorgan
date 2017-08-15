721e655f99cc4d033f4f4966d557c94f66536a06
Law <zarakiblack@gmail.com>
2017-08-15 13:56:12 -0300

    select curso

--- js/default.js
+++ js/default.js
@@ -12,7 +12,6 @@ function scroll() {
     $("main").fullpage({
         anchors: ['bem-vindo','a-iniciativa','o-curso','grade-curricular','linha-do-tempo','inscreva-se','nossa-equipe','nossos-parceiros','contate-nos'],
         menu: '#menu',
-        scrollOverflowReset: true,
         onLeave: function (anchorIndex, index) {
             if (index === 5) {
                 $(".fp-scrollable").data('iscrollInstance').scrollTo(0, 0);
@@ -33,26 +32,42 @@ function menu() {
 
 function curso() {
     $(".grades").on("click",function(){
+        $(".grades svg line,.grades svg path,.grades svg circle,.grades svg polyline").removeAttr('style');
+        $(".grades svg line,.grades svg path,.grades svg circle,.grades svg polyline").css({
+            "will-change": "fill,stroke",
+            "transition" : "all .5s"
+        });
         switch ($(this).data("grade")){
             case 'mobile':
-                $(".e110ae3c-3bd4-46cc-ba7e-afe58f8ac555,.824ae0ee-1a7f-4baf-ae7a-7240550d8a25,.82096e82-3989-4728-ac0b-46d2741652ec").css("stroke","#fff");
-                $(".407a2a06-c78b-41c3-b335-6e64d5e03ed8").css("stroke","#634893");
-                $(".33b0ff79-7e11-45b9-930e-11f75c62810c").css({
+                $(".79467428-59bb-423e-94d6-724c8d65da3e,.bad627ad-1ab8-4cf9-9bd1-57c7d119eb5a,.fcf334ac-c5b2-4203-801c-e581911a8b6c").css("stroke","#fff");
+                $(".8938a608-9343-4f50-8f77-8bffbfac7f82").css("stroke","#634893");
+                $(".dd93b2e8-d58a-4e5a-a524-503aed984d16").css({
                     "stroke":"transparent",
                     "fill":"#634893"
-                })
+                });
                 break;
             case 'web':
-                $(".15d8a20b-15b2-44bf-87e0-5d4a39094ddb,.f1549859-ac4c-451f-8678-e3b89bcc96cb").css("stroke","#51b9b5");
-                $(".126865aa-eafc-4ca2-9cda-0b202f09e0ba,.f8e6edab-d375-45d2-bedb-d46693705fdc,.32c7e67c-07fb-42f7-8b20-e62f393691b2,.f8e6edab-d375-45d2-bedb-d46693705fdc").css("stroke","#fff");
-                $(".a0d1b0f9-69f3-49fd-9826-d04c84d6e0e8").css({
+                $(".a5a9468b-d8eb-4f93-a3a9-ff02dd93d9da,.bfd18684-fe00-48a5-b318-a70120889bde").css("stroke","#51b9b5");
+                $(".ebe3d445-6749-4bf0-b20c-f4f535ea2758,.06625f60-5548-4785-9065-9616281eeb7a,.da6216a5-663b-49bf-b350-d2f576cfd871").css("stroke","#fff");
+                $(".a9a981be-c51f-487d-acc3-0e773c4f582f").css({
                     "stroke":"transparent",
                     "fill":"#51b9b5"
-                })
+                });
                 break;
             case 'back':
+                $(".44d5883a-be9c-42ca-923e-070f2b15f68d").css("stroke","#d8167d");
+                $(".67bf3dba-ad89-4e02-85c9-3329e1f39750,.05b6ef4e-cbfc-44b0-a14c-31df405d6aa0,.3de6f27a-3817-49d7-a6f7-e1030e921155").css("stroke","#fff");
+                $(".1a095f3d-aa3c-4402-93bc-0064652cfa38").css({
+                    "stroke":"transparent",
+                    "fill":"#d8167d"
+                });
                 break;
             case 'iot':
+                $(".e0e7de21-cdc6-4a8f-a756-80967adcbd15,.b6937b7b-368a-4293-ac05-56e406e5731d,.c6c95c6b-4562-480d-b26d-69494a54b146,.2909dbcf-e690-42b5-af1a-8a49a3530e1e").css("stroke","#fff");
+                $(".aaff7a77-8c67-4002-b7b6-ce234008cf91").css({
+                    "stroke":"transparent",
+                    "fill":"#dd5833"
+                });
                 break;
         }
         $(".curso").css('opacity','0');
